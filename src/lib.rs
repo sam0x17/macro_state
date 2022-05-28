ba5e0a0239191008574a1ab4eff631cb336597fe
Sam Johnson <sam@durosoft.com>
2022-05-28 18:32:09 -0400

    write_state! macro working

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,72 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::PathBuf;
 
-// write_state!(key, value)
+fn state_file_path(key: &str) -> PathBuf {
+    let filename = format!("macro_state_{}", key);
+    let mut buf = PathBuf::new();
+    buf.push(env!("MACRO_STATE_DIR"));
+    buf.push(filename.as_str());
+    buf
+}
+
+/// Writes the specified value as the state for the specified key
+/// # Example
+/// write_state!("my key", "some value");
 #[proc_macro]
-pub fn write_state(_items: TokenStream) -> TokenStream {
-    println!("{}", env!("MACRO_STATE_DIR"));
+pub fn write_state(items: TokenStream) -> TokenStream {
+    let mut key = String::new();
+    let mut value = String::new();
+    let mut i = 0;
+    for item in items {
+        let token = item.to_string();
+        match i {
+            0 => {
+                // first token
+                match item {
+                    proc_macro::TokenTree::Literal(literal) => {
+                        key = literal.to_string();
+                    }
+                    _ => {
+                        panic!("unexpected token {}", token);
+                    }
+                }
+            }
+            1 => {
+                // second token
+                match item {
+                    proc_macro::TokenTree::Punct(punc) => {
+                        if punc.as_char() != ',' {
+                            panic!("unexpected token {}", token);
+                        }
+                    }
+                    _ => {
+                        panic!("unexpected token {}", token);
+                    }
+                }
+            }
+            2 => {
+                // third token
+                match item {
+                    proc_macro::TokenTree::Literal(literal) => {
+                        value = literal.to_string();
+                    }
+                    _ => {
+                        panic!("unexpected token {}", token);
+                    }
+                }
+            }
+            _ => {
+                panic!("unexpected token {}", token);
+            }
+        }
+        i += 1;
+    }
+    println!("key: {}, value: {}", key, value);
+    let mut file = File::create(state_file_path(key.as_str())).unwrap();
+    file.write_all(value.as_bytes()).unwrap();
     "println!(\"hello world\");".parse().unwrap()
 }
