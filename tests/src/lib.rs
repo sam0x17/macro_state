48c8d598a35752d7dcc74c99b0eda2ee1698d199
Sam Johnson <sam@durosoft.com>
2022-05-30 02:25:45 -0400

    tests working

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,8 +1,25 @@
+#[macro_use]
+extern crate macro_state;
+
+write_state!("top_of_file", "value 1");
+
 #[cfg(test)]
 mod tests {
+    write_state!("top of module", "value 2");
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_write_state() {
+        write_state!("top of method", "value 3");
+        assert_eq!(read_state!("top_of_file"), "value 1");
+        assert_eq!(read_state!("top of module"), "value 2");
+        assert_eq!(read_state!("top of method"), "value 3");
+    }
+
+    #[test]
+    fn test_rewriting_state() {
+        write_state!("key 1", "value 4");
+        assert_eq!(read_state!("key 1"), "value 4");
+        write_state!("key 1", "value 5");
+        assert_eq!(read_state!("key 1"), "value 5");
     }
 }
